e523bfd949b15865294e56c4625312c2280617b4
Alzahsi1212 <alzahsi1212@gmail.com>
2023-05-21 19:11:51 +0800

    Mengisi nilai atribut

--- src/main/java/praktek01/MahasiswaAksi.java
+++ src/main/java/praktek01/MahasiswaAksi.java
@@ -11,5 +11,9 @@ package praktek01;
 public class MahasiswaAksi {
     public static void main(String[] args) {
         Mahasiswa Ansari = new Mahasiswa();
+        
+        Ansari.nama="M. Alzahsi Ansari";
+        Ansari.NPM="2110010399";
+        Ansari.IPK=3.4;
     }
 }
